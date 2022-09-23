2b7b23a51ea219b9cff498d21b8b93fb77e27643
Rodrigo Soares <meuemail.rodrigodev@gmail.com>
2022-09-22 22:32:14 -0300

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Hero from './components/Hero'
+import Submenu from './components/Submenu'
+
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  )
+}
+
+export default App
